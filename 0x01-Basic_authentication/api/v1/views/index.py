0dc9530b92d196b6930b87d44c6ccec2eb161f5c
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-07 12:52:27 +0300

    Error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> str:
     This endpoint raises a 401 Unauthorized error
     """
     abort(401)
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    This endpoint raises a 403 Forbidden error
+    """
+    abort(403)
