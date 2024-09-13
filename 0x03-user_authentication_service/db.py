53fd6ba2556996f758e61f80695b3d573b07f377
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-13 21:23:23 +0300

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError, NoResultFound, SQLAlchemyError
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
 
 
@@ -52,6 +53,7 @@ class DB:
         """
         session = self._session
         try:
+            # Querying using filter_by with **kwargs
             user = session.query(User).filter_by(**kwargs).first().one()
             return user
         except InvalidRequestError:
