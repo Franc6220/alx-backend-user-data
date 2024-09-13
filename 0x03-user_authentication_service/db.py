eded5de3dcdde1e8703c3334f5dc9988d78095f5
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-13 19:49:47 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,7 @@
 DB module
 """
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import SQLAlchemyError
 from user import User
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +12,7 @@ from sqlalchemy.orm import sessionmaker
 from user import Base
 
 class DB:
-    """DB class
+    """DB class to manage user records
     """
 
     def __init__(self) -> None:
@@ -38,6 +39,7 @@ class DB:
             session = self._session
             session.add(new_user)
             session.commit()
+            session.refresh(new_user)
             return new_user
         except SQLAlchemyError:
             session.rollback()
