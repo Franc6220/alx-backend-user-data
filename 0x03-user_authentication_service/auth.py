5e0b0a96c7c315ec9891d22fb84c6f2f47b031cf
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-14 11:12:09 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """
-Auth module for password hashing
+Auth module for password hashing and to interact with the authentication database.
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from typing import Optional
 
 def _hash_password(password: str) -> bytes:
     """
@@ -16,3 +20,34 @@ def _hash_password(password: str) -> bytes:
     """
     # Generate a salt and hash the password using bcrypt
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Optional[User]:
+        """
+        Registers a user with an email and password.
+        If the email is already in use, raise a ValueError.
+
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
+
+        Returns:
+            User: The newly created user.
+
+        Raises:
+            ValueError: If the user with the email already exists.
+        """
+        try:
+            # Try to find if a user already exists with the provided email
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # If no result is found, we can proceed to create the user
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
