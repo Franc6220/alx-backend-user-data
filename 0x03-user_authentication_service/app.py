1f988a68607262f503da14c271272523f76d4cc1
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-15 13:14:14 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic Flask app for returning a JSON response and for user registration
 """
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -35,6 +35,27 @@ def register_user():
     db.session.commit()
 
     return jsonify({"email": email, "message": "user created"}), 201
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+    Handle POST requests to /sessions for user login.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        abort(400, description="Missing email or password")
+
+    if auth.valid_login(email, password):
+        session_id = auth.create_session(email)
+        response = make_response(
+                jsonify({"email": email, "message": "logged in"})
+                )
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401, description="Invalid login credentials")
 
 
 if __name__ == "__main__":
